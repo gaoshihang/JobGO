bb5e6fa59731fecb1972c94c1ad4fcd334f8bce5
gaoshihang <472024406@qq.com>
2019-10-25 14:19:12 +0800

    Update ShortestSubArray.java

--- 一.算法/1.数组/最短无序连续子数组/ShortestSubArray.java
+++ 一.算法/1.数组/最短无序连续子数组/ShortestSubArray.java
@@ -43,3 +43,91 @@
         }
         return res;
     }
+    
+2.解法二：更好的暴力解法
+解法：我们基于选择排序使用如下想法：遍历数组中每一个nums[i]，在i<j<n，n为数组长度的区间里，用nums[j]和nums[i]做比较。
+若nums[j] < nums[i]，则i和j上的数字都不在正确的位置，这两个元素标记着当前无序数组的边界。
+遍历所有的nums[i]，找到最左边不正确位置的nums[i]，这标记着最左边界nums[l]；找到最右边不正确位置的nums[j]，标记着最右边界r。
+时间复杂度：O(n^2)
+
+代码如下：
+public static int findUnsortedSubarray(int[] nums){
+    int l = nums.length;
+    int r = 0;
+    for(int i = 0; i < nums.length-1; i++){
+        for(int j = i+1; j < nums.length; j++){
+            if(nums[j] < nums[i]){
+                l = Math.min(l, i);
+                r = Math.min(r, j);
+            }
+        }
+    }
+    return r - 1 < 0 ? 0 : r - l + 1;
+}
+
+3.解法三：排序
+先对原始的数组进行排序，排序后与原数组进行比较，得出最左边和最右边的不匹配元素。
+时间复杂度：O(nlogn)，排序
+空间复杂度：O(n)，拷贝原数组
+代码如下：
+public static int findUnsortedSubarray(int[] nums){
+    int[] snums = nums.clone();
+    Arrays.sort(snums);
+    int start = snums.length;
+    int end = 0;
+    for(int i = 0; i < snums.length; i++){
+        if(nums[i] != snums[i]){
+            start = Math.min(start, i);
+            end = Math.max(end, i);
+        }
+    }
+    
+    return start < end ? end - start + 1 : 0;
+}
+
+4.解法四：使用栈
+从头遍历数组nums，如果一直是升序的，则将其下标压入栈中，直到遇到一个降序的数字nums[j]，不断的从栈顶弹出元素，直到栈顶数组大小小于这个数字，
+若此时栈顶元素的下标为k，则得到nums[j]应该在的下标位置，即为k+1。
+我们重复这一过程并遍历完整个数组，这样我们可以找到最小的 k， 它也是无序子数组的左边界。
+类似的，我们逆序遍历一遍 numsnums 数组来找到无序子数组的右边界。
+这一次我们将降序的元素压入栈中，如果遇到一个升序的元素，我们像上面所述的方法一样不断将栈顶元素弹出，直到找到一个更大的元素，
+以此找到无序子数组的右边界。
+时间复杂度：O(n)
+空间复杂度：O(n)
+
+代码如下：
+public static int findUnsortedSubarray(int[] nums){
+    Stack<Integer> stack = new Stack<>();
+    int l = nums.length;
+    int r = 0;
+    for(int i = 0; i < nums.length; i++){
+        while(!stack.isEmpty() && nums[stack.peek()] > nums[i]){
+            l = Math.min(l, stack.pop());
+        }
+        stack.push(i);
+    }
+    
+    stack.clear();
+    for(int i = 0; i < nums.length; i++){
+        while(!stack.isEmpty() && nums[stack.peek()] < nums[i]){
+            r = Math.max(r, stack.pop());
+        }
+        stack.push(i);
+    }
+    
+    return r - 1 > 0 ? r - l + 1 : 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
